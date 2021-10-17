c8246a46c425c70680aa0237a2d11c09cdd6e56c
edgardev01 <sanchezmauricioedgar01@gmail.com>
2021-10-17 13:09:03 -0500

    expandiendo basicos

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,27 @@
+import { Component,Input } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+
+})
+export class MainPageComponent {
+ 
+
+  
+
+  nuevo: Personaje = {
+    nombre: 'Maestro',
+    poder: 2000
+  }
+
+
+
+  constructor( ) {
+    
+  }
+
+}
